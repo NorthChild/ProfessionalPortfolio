9ac3133f5cac0a8c61a68546ac43395b3158b4b4
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-01-29 11:50:19 +0100

    Add files via upload

---
+++ Personal Portfolio/JS/portfolio.js
@@ -0,0 +1,10 @@
+/*
+// TO DO //
+
+- create a color button that changes style (light/dark)
+- MAYBE create a pop up for the projects, giving a short description
+first see by doing the normal way
+- put icons at the top that lead to the sections, put a pop up displaying what sections it is moving to
+- link the more button to the github repository
+// TO DO //
+*/
